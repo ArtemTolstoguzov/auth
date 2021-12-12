e66a5fad5efd56e840e38272c709f90931fb4a2c
Artem <artemtolstoguzov02@gmail.com>
2021-12-12 16:44:06 +0500

    9 done

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4;
 
 namespace IdentityServer
 {
@@ -12,7 +13,13 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
             { 
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource("photos_app", "Web Photos", new []
+                {
+                    "role", "subscription", "testing"
+                })
             };
 
         public static IEnumerable<ApiResource> Apis =>
@@ -22,6 +29,7 @@ namespace IdentityServer
                 {
                     Scopes = { "photos" }
                 }
+                
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -43,6 +51,38 @@ namespace IdentityServer
 
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = { "photos" }
+                },
+                new()
+                {
+                    ClientId = "Photos App by OIDC",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+        
+                    // NOTE: показывать ли пользователю страницу consent со списком запрошенных разрешений
+                    RequireConsent = false,
+
+                    // NOTE: куда отправлять после логина
+                    RedirectUris = { "https://localhost:5001/signin-passport" },
+                    // NOTE: куда предлагать перейти после логаута
+                    PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-passport" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        
+                        // NOTE: Позволяет запрашивать id token
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        // NOTE: Позволяет запрашивать профиль пользователя через id token
+                        IdentityServerConstants.StandardScopes.Profile,
+                        // NOTE: Позволяет запрашивать email пользователя через id token
+                        IdentityServerConstants.StandardScopes.Email,
+                        "photos_app"
+                    },
+
+                    // NOTE: Надо ли добавлять информацию о пользователе в id token при запросе одновременно
+                    // id token и access token, как это происходит в code flow.
+                    // Либо придется ее получать отдельно через user info endpoint.
+                    AlwaysIncludeUserClaimsInIdToken = true,
                 }
             };
     }
